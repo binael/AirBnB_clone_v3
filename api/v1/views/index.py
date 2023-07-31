ad14a157ace668984931f4e7f8bce7cffc896cf8
binael <nbinael@yahoo.com>
2023-08-01 00:27:46 +0100

    summary of items

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,18 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City, "places": Place, 
+           "reviews": Review, "states": State, "users": User}
+
 
 
 @app_views.route('/status')
@@ -11,3 +23,14 @@ def status():
     Returns the status ok
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    mydict = {}
+    for key, value in classes.items():
+        num = storage.count(value)
+        if (num):
+            mydict[key] = num
+
+    return jsonify(mydict)
